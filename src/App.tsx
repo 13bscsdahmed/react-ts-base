48551c2525f80d603588ab41e6149fa32ecb3e11
hamza <hamzasajid424@gmail.com>
2022-02-18 15:17:03 +0500

    eslint-prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import './locales/i18n';
-import React, { Suspense, useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 import { useLocation, useRoutes } from 'react-router';
-import Routes from './routes';
 import { injectReducer } from '@store/store';
 import { storeConfig } from '@store/store.config';
 import ConfigSlice from '@store/slices/config/ConfigSlice';
 import { useDispatch } from 'react-redux';
 import { configActionTypes } from '@store/slices';
-
+import Routes from './routes';
 
 function App() {
-  const dispatch = useDispatch();
-  const location = useLocation();
-  let appRoutes = useRoutes(Routes);
-  useEffect(() => {
-    injectReducer(storeConfig.slices.config, ConfigSlice.reducer);
-    dispatch({type: configActionTypes.FETCH_CONFIG});
-    console.log('location', location);
-  }, [location])
-  return (
-    <div className='main'>
-      <Suspense fallback="Loading ...">
-        {appRoutes}
-      </Suspense>
-    </div>
-  );
+	const dispatch = useDispatch();
+	const location = useLocation();
+	const appRoutes = useRoutes(Routes);
+
+	useEffect(() => {
+		injectReducer(storeConfig.slices.config, ConfigSlice.reducer);
+		dispatch({ type: configActionTypes.FETCH_CONFIG });
+		console.log('location', location);
+	}, [location, dispatch]);
+
+	return (
+		<div className='main'>
+			<Suspense fallback='Loading ...'>{appRoutes}</Suspense>
+		</div>
+	);
 }
 
 export default App;
